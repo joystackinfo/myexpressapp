e6ea7ee25ede0b712bc0dd42fee2ec6e4c57bcce
joystackinfo <joynelsoninfo@gmail.com>
2025-09-16 20:43:19 +0100

    created a server

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+// basic route
+app.get("/", (req, res) => {
+  res.send("Hello, Express! 🚀");
+});
+
+// start server
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
