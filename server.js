6687029d8a5283ac93d1c2a8d0940785cc969c5c
joystackinfo <joynelsoninfo@gmail.com>
2025-09-16 21:14:37 +0100

    added another file

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ const PORT = 3000; // define a port number
 
 // basic route
 app.get("/" , (req, res) => { // handle GET request to root URL (/ is the homepage)
-  res.send("Hello, Express! 🚀"); // send a response
+  res.send("Hello, Express! 🚀 i am learning express"); // send a response
 });
 
+app.get("/about", (req, res) => { // handle GET request to /about URL
+    res.send("This is about the web page");
+});
 // start server
 app.listen(PORT, () => { // start the server and listen on the defined port
   console.log(`Server running at http://localhost:${PORT}`); // log a message when the server is running
