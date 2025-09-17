b654eebca768b0b087663eed163abfbc3b855014
joystackinfo <joynelsoninfo@gmail.com>
2025-09-17 14:14:00 +0100

    setup a static folder

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require("express");
+const path = require("path");
 const app = express(); // create an Express application
 const PORT = 3000; // define a port number
 
-// basic route
-app.get("/" , (req, res) => { // handle GET request to root URL (/ is the homepage)
-  res.send("Hello, Express! 🚀 i am learning express"); // send a response
-});
+//SETUP STATIC FOLDER
 
-app.get("/about", (req, res) => { // handle GET request to /about URL
-    res.send("This is about the web page");
-});
+app.use(express.static(path.join(__dirname, "public"))); // serve static files from the "public" directory
+
+// // basic route
+// app.get("/" , (req, res) => { // handle GET request to root URL (/ is the homepage)
+//   res.sendFile(path.join(__dirname, "public", "index.html")); // send index.html file as response
+// });
+
+// app.get("/about", (req, res) => { // handle GET request to /about URL
+//     res.sendFile(path.join(__dirname, "public", "about.html")); // send about.html file as response
+// });
 // start server
 app.listen(PORT, () => { // start the server and listen on the defined port
   console.log(`Server running at http://localhost:${PORT}`); // log a message when the server is running
