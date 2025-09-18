1e2e6ef35e96010429927c6f45d7f3ae8e444050
joystackinfo <joynelsoninfo@gmail.com>
2025-09-18 20:49:03 +0100

    added a comment

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const path = require("path");
+const path = require("path"); // create a path appplication
 const app = express(); // create an Express application
 const PORT = 3000; // define a port number
 
