65fd6c24a85a28559d37da1091bec98294e6faa2
joystackinfo <joynelsoninfo@gmail.com>
2025-09-23 17:25:03 +0100

    updated server.js

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const posts = require('./routes/posts');
+const posts = require('./routes/posts'); // import your posts router
 const PORT = process.env.PORT || 3000;
 
-// Body parser middleware (put these BEFORE routes)
+// Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Routes
 app.use('/api/posts', posts);
-
 
 // Start server
 app.listen(PORT, () => {
